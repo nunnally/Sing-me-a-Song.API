e620e7be5b6c4780a259c43c56b2a383205d2e55
Guga <contato.gustavobarbosa@outlook.com>
2021-08-03 17:27:44 -0300

    Preparando revisão do código do César

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-import recommendation from "./recommendation";
-
-
-const routes = Router();
-
-routes.use("/recommendation", recommendation);
-
-export default routes;
